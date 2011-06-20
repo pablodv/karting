2c3b5baf1643999061b20e33794718b4a709791d
Pablo <pablo.moreiramora@gmail.com>
2011-06-20 16:14:02 -0300

    Apply style

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Karting::Application.routes.draw do
-  devise_for :admins, :path => :admin
+  devise_for :admins, :path => :admin, :controllers => { :sessions => "admins/users/sessions" }
 
   namespace :admins, :path => :admin do
     root :to => "clients#index"
     resources :clients
   end
 
-  root :to => "clients#index"
+  root :to => "admins/clients#index"
 end
